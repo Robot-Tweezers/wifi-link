6dc2a18f26118ac6578c09a4b41e3307394891f9
TyKeeling <tylerwkeeling@gmail.com>
2022-04-01 18:25:55 -0700

    wifi -> protobuf -> serial loopback

--- src/main.cpp
+++ src/main.cpp
@@ -4,120 +4,102 @@
 #include <WiFi.h>
 #include <config.hpp>
 #include <protobuf.hpp>
+#include <HardwareSerial.h>
 
 using namespace RobotTweezers;
 
 const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASS;
 
-/**
- * @brief Wait for an ackowledgement from the Teensy before beginning program
- *
- */
-static void WaitForConnection(void)
-{
-    UartConnection connection_msg, response;
-    int8_t attempts = 5;
+uint8_t inbuffer[128];
+uint8_t outbuffer[128];
+bool status;
+int message_length;
 
-    response.id = ESP32_ID;
+WiFiServer wifiServer(80);
 
-    while (attempts--)
-    {
-        if (Protobuf::UartRead(&Serial, &connection_msg))
-        {
-            if (connection_msg.id == TEENSY_ID)
-            {
-                digitalWrite(LED_BUILTIN, HIGH);
-                Protobuf::UartWrite(&Serial, &response);
-                break;
-            }
-        }
+HardwareSerial s1 = HardwareSerial(1);
 
-        delay(1000);
-    }
-}
-
-static void OnStart(void)
-{
-    //String type = (ArduinoOTA.getCommand() == U_FLASH) ? "sketch" : "filesystem";
-    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-    //Serial.println("Start updating " + type);
-}
-
-static void OnEnd(void)
-{
-    //Serial.println("\nEnd");
-}
-
-static void OnProgress(unsigned int progress, unsigned int total)
-{
-    //Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-}
-
-static void OnError(ota_error_t error)
-{
-    /*
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR)
-        Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR)
-        Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR)
-        Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR)
-        Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR)
-        Serial.println("End Failed");
-    */
-}
-
-void setup()
-{
-    Serial.begin(PROTOBUF_INTERFACE_BAUDRATE);
-
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, LOW);
-
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("iPhone", "tylertylertyler");
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
     delay(1000);
-    WaitForConnection();
-
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-    while (WiFi.waitForConnectResult() != WL_CONNECTED)
-    {
-        //Serial.println("Connection Failed! Rebooting...");
-        delay(5000);
-        ESP.restart();
-    }
-
-    // Port defaults to 3232
-    // ArduinoOTA.setPort(3232);
-
-    // Hostname defaults to esp3232-[MAC]
-    // ArduinoOTA.setHostname("myesp32");
-
-    // No authentication by default
-    // ArduinoOTA.setPassword("admin");
-
-    // Password can be set with it's md5 value as well
-    // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-    // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
-    ArduinoOTA
-        .onStart(OnStart)
-        .onEnd(OnEnd)
-        .onProgress(OnProgress)
-        .onError(OnError);
-
-    ArduinoOTA.begin();
-
-    //Serial.println("Ready");
-    //Serial.print("IP address: ");
-    //Serial.println(WiFi.localIP());
+  }
+  Serial.println(WiFi.localIP());
+  wifiServer.begin();
 }
 
-void loop()
-{
-    ArduinoOTA.handle();
-    delay(100);
+void decode_orientation_msg(unsigned char *buffer, int len) {
+	OrientationMsg d = OrientationMsg_init_zero;
+	pb_istream_t istream = pb_istream_from_buffer(buffer, len);
+	status = pb_decode(&istream, OrientationMsg_fields, &d);
+
+	if (!status) {
+		printf("Decoding Failed: %s\n", PB_GET_ERROR(&istream));
+		return;
+	}
+
+
+
+    bool success = Protobuf::UartWrite(&s1, &d);
+
+	if (!success) {
+		printf("Decoding Failed: %s\n", PB_GET_ERROR(&istream));
+		return;
+	} else {
+        OrientationMsg overserial = OrientationMsg_init_zero;
+        Protobuf::UartRead(&s1, &overserial);
+        Serial.println("Data:");
+        Serial.write("Roll: ");
+        Serial.println(d.roll);
+        Serial.write("Pitch: ");
+        Serial.println(d.pitch);
+        Serial.write("Yaw: ");
+        Serial.println(d.yaw);
+    }
+}
+
+void wifi_loop() {
+	WiFiClient client = wifiServer.available();
+
+	int size;
+
+	if (client) {
+		while (client.connected()) {
+			size = client.available();
+
+			if (size > 0) {
+                Serial.print("Connected. Size: ");
+                Serial.println(size);
+
+				client.readBytes(inbuffer, sizeof(inbuffer));
+
+                decode_orientation_msg(inbuffer, size);
+
+				client.write(inbuffer, sizeof(inbuffer));
+			}
+
+			delay(10);
+		}
+
+		client.stop();
+		Serial.println("Client disconnected");
+	}
+
+	Serial.print("Waiting for client... IP address: ");
+	Serial.println(WiFi.localIP());
+	delay(1000);
+}
+
+void setup() {
+    s1.begin(9600, SERIAL_8N1, GPIO_NUM_16, GPIO_NUM_17);
+    Serial.begin(9600);
+	initWiFi();
+}
+
+void loop() {
+	wifi_loop();
 }
